661613f07ca3f8db24a3870cd985c0ff79691e2e
Raze <33286619+officialraze@users.noreply.github.com>
2018-11-21 15:00:31 +0100

    webproject new
    - player changes
    - color changes
    - add images for preview
    - added player to dj site

--- index.php
+++ index.php
@@ -21,7 +21,25 @@
 		<script type="text/javascript" src="js/functions.js"></script>
 
 		<!-- Icons-->
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+
+		<!-- <link rel="stylesheet" type="text/css" href="css/foundation.min.css"/> -->
+
+		<!-- Include font -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+
+		<script type="text/javascript" src="/js/jquery.js"></script>
+		<script type="text/javascript" src="/js/foundation.min.js"></script>
+		<script type="text/javascript" src="/js/functions.js"></script>
+
+		<!-- Include Amplitude JS -->
+		<script type="text/javascript" src="/js/amplitude.js"></script>
+
+		<!-- Include Style Sheet -->
+		<!-- <link rel="stylesheet" type="text/css" href="css/app.css"/> -->
+
+
 
 		<script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("U need a modern browser.");</script>
 	</head>
@@ -98,6 +116,118 @@
 						<p>His entry had disturbed the water, and now he discerned a number of dim corpses making for him and wagging their heads and swaying their bodies like sleepy people trying to dance. His senses forsook him, and in that condition he was drawn to the surface. He was put to bed at home, and was soon very ill. During some days he had seasons of delirium which lasted several hours at a time; and while they lasted he talked Kanaka incessantly and glibly; and Kanaka only. He was still very ill, and he talked to me in that tongue; but I did not understand it, of course. The doctor-books tell us that cases like this are not uncommon. Then the doctors ought to study the cases and find out how to multiply them. Many languages and things get mislaid in a person’s head, and stay mislaid for lack of this remedy.</p>
 						<p>Several of our passengers belonged in Honolulu, and these were sent ashore; but nobody could go ashore and return. There were people on shore who were booked to go with us to Australia, but we could not receive them; to do it would cost us a quarantine-term in Sydney. They could have escaped the day before, by ship to San Francisco; but the bars had been put up, now, and they might have to wait weeks before any ship could venture to give them a passage any whither. And there were hardships for others. An elderly lady and her son, recreation-seekers from Massachusetts, had wandered westward, further and further from home, always intending to take the return track, but always concluding to go still a little further; and now here they were at anchor before Honolulu positively their last westward-bound indulgence—they had made up their minds to that—but where is the use in making up your mind in this world? It is usually a waste of time to do it. These two would have to stay with us as far as Australia. Then they could go on around the world, or go back the way they had come; the distance and the accommodations and outlay of time would be just the same, whichever of the two routes they might elect to take. Think of it: a projected excursion of five hundred miles gradually enlarged, without any elaborate degree of intention, to a possible twenty-four thousand. However, they were used to extensions by this time, and did not mind this new one much.</p>
 					</div>
+					<div class="music_player">
+						<div id="white-player">
+				          <div class="white-player-top">
+				            <div class="grid-x">
+				              <div class="large-3 medium-3 small-3 cell">
+
+				              </div>
+				              <div class="large-6 medium-6 small-6 cell">
+				                <span class="now-playing">Läuft gerade</span>
+				              </div>
+				              <div class="large-3 medium-3 small-3 cell">
+				                <img src="img/show-playlist.svg" class="show-playlist"/>
+				              </div>
+				            </div>
+				          </div>
+
+				          <div id="white-player-center">
+				            <img amplitude-song-info="cover_art_url" amplitude-main-song-info="true" class="main-album-art"/>
+
+				            <div class="song-meta-data">
+				              <span amplitude-song-info="name" amplitude-main-song-info="true" class="song-name"></span>
+				              <span amplitude-song-info="artist" amplitude-main-song-info="true" class="song-artist"></span>
+				            </div>
+
+				            <div class="time-progress">
+				              <div class="grid-x">
+				                <div class="large-12 medium-12 small-12 cell">
+				                  <div id="progress-container">
+				                    <input type="range" class="amplitude-song-slider" amplitude-main-song-slider="true"/>
+				  				          <progress id="song-played-progress" class="amplitude-song-played-progress" amplitude-main-song-played-progress="true"></progress>
+				  				          <progress id="song-buffered-progress" class="amplitude-buffered-progress" value="0"></progress>
+				                  </div>
+				                </div>
+				              </div>
+
+				              <div class="grid-x">
+				                <div class="large-6 medium-6 small-6 cell">
+				                  <span class="current-time">
+				  				          <span class="amplitude-current-minutes" amplitude-main-current-minutes="true"></span>:<span class="amplitude-current-seconds" amplitude-main-current-seconds="true"></span>
+				  				        </span>
+				                </div>
+				                <div class="large-6 medium-6 small-6 cell">
+				                  <span class="duration">
+				  				          <span class="amplitude-duration-minutes" amplitude-main-duration-minutes="true"></span>:<span class="amplitude-duration-seconds" amplitude-main-duration-seconds="true"></span>
+				  				        </span>
+				                </div>
+				              </div>
+				            </div>
+				          </div>
+
+				          <div id="white-player-controls">
+				            <div class="grid-x">
+				              <div class="large-12 medium-12 small-12 cell">
+				                <div class="amplitude-shuffle amplitude-shuffle-off" id="shuffle"></div>
+				                <div class="amplitude-prev" id="previous"></div>
+				                <div class="amplitude-play-pause" amplitude-main-play-pause="true" id="play-pause"></div>
+				                <div class="amplitude-next" id="next"></div>
+				                <div class="amplitude-repeat" id="repeat"></div>
+				              </div>
+				            </div>
+				          </div>
+
+				          <div id="white-player-playlist-container">
+				            <div class="white-player-playlist-top">
+				              <div class="grid-x">
+				                <div class="large-3 medium-3 small-3 cell">
+
+				                </div>
+				                <div class="large-6 medium-6 small-6 cell">
+				                  <span class="queue">Warteschlange</span>
+				                </div>
+				                <div class="large-3 medium-3 small-3 cell">
+				                  <img src="img/close.svg" class="close-playlist"/>
+				                </div>
+				              </div>
+				            </div>
+
+				            <div class="white-player-up-next">Nächster Song</div>
+
+				            <div class="white-player-playlist">
+				              <div class="white-player-playlist-song amplitude-song-container amplitude-play-pause" amplitude-song-index="0">
+				                <img src="img/covers/notalone.jpg"/>
+
+				                <div class="playlist-song-meta">
+				                  <span class="playlist-song-name">We're Not Alone</span>
+				                  <span class="playlist-artist-album">Virtual Riot &bull; We're Not Alone - EP</span>
+				                </div>
+				              </div>
+				            </div>
+
+				            <div class="white-player-playlist-controls">
+				              <img amplitude-song-info="cover_art_url" amplitude-main-song-info="true" class="playlist-album-art"/>
+
+				              <div class="playlist-controls">
+				                <div class="grid-x">
+				                  <div class="large-6 medium-6 small-6 cell">
+				                    <span amplitude-song-info="name" amplitude-main-song-info="true" class="song-name"></span>
+				                    <span amplitude-song-info="artist" amplitude-main-song-info="true" class="song-artist"></span>
+				                  </div>
+				                  <div class="large-6 medium-6 small-6 cell">
+				                    <div class="playlist-control-wrapper">
+				                      <div class="amplitude-prev" id="playlist-previous"></div>
+				                      <div class="amplitude-play-pause" amplitude-main-play-pause="true" id="playlist-play-pause"></div>
+				                      <div class="amplitude-next" id="playlist-next"></div>
+				                    </div>
+				                  </div>
+				                </div>
+				              </div>
+				            </div>
+				          </div>
+				        </div>
+					</div>
 				</div><!-- /content__item -->
 				<div class="content__item">
 					<div class="content__item-intro">
@@ -164,7 +294,7 @@
 						<p>Several of our passengers belonged in Honolulu, and these were sent ashore; but nobody could go ashore and return. There were people on shore who were booked to go with us to Australia, but we could not receive them; to do it would cost us a quarantine-term in Sydney. They could have escaped the day before, by ship to San Francisco; but the bars had been put up, now, and they might have to wait weeks before any ship could venture to give them a passage any whither. And there were hardships for others. An elderly lady and her son, recreation-seekers from Massachusetts, had wandered westward, further and further from home, always intending to take the return track, but always concluding to go still a little further; and now here they were at anchor before Honolulu positively their last westward-bound indulgence—they had made up their minds to that—but where is the use in making up your mind in this world? It is usually a waste of time to do it. These two would have to stay with us as far as Australia. Then they could go on around the world, or go back the way they had come; the distance and the accommodations and outlay of time would be just the same, whichever of the two routes they might elect to take. Think of it: a projected excursion of five hundred miles gradually enlarged, without any elaborate degree of intention, to a possible twenty-four thousand. However, they were used to extensions by this time, and did not mind this new one much.</p>
 					</div>
 				</div><!-- /content__item -->
-				<button class="content__close">Schliessen</button>
+				<button class="content__close"><i class="fas fa-times"></i></button>
 				<svg class="content__indicator icon icon--caret"><use xlink:href="#icon-caret"></use></svg>
 			</div>
 		</main>
@@ -177,7 +307,18 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
-		<script>
+		<script type="text/javascript">
+		  Amplitude.init({
+			"songs": [
+			  {
+				"name": "We're Not Alone",
+				"artist": "Virtual Riot",
+				"album": "We're Not Alone - EP",
+				// "url": "../songs/Ancient Astronauts - Risin' High (feat Raashan Ahmad).mp3",
+				"cover_art_url": "img/covers/notalone.jpg"
+			  },
+			]
+		  });
 		</script>
 	</body>
 </html>
